e01717817fd688a84f7e556afef064140eb73e8a
MaryOwino <mariaowino2@gmail.com>
2023-05-08 10:10:07 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * strstr - Entry point
+ * _strstr - Entry point
  * @haystack: input
  * @needle: input
  * Return: Always 0 (Success)
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 		char *one = haystack;
 		char *two = needle;
 
-		while (*one == *two && *two !* '\0')
+		while (*one == *two && *two != '\0')
 		{
 			one++;
 			two++;
